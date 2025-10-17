113d6f3389f0c471c7ed6832e53b7997dabce466
Mohammad F Naz <142484650+FarheenMohammad@users.noreply.github.com>
2025-10-17 10:53:34 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6366F1",
+        accent: "#FACC15",
+      },
+    },
+  },
+  plugins: [],
+};
